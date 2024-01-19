520208d8922ee2052b8fef68faebd313141f4bde
Hugo Perez <hugoperez469@gmail.com>
2024-01-19 14:46:53 -0600

    Se añadio Router, con homestack

---
+++ router/AppNavigator.js
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeStackNavigator from './HomeStackNavigator';
+import LoginScreen from '../views/LoginScreenV/LoginScreen';
+//import ProfileScreen from '../views/profileScreen/ProfileScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="HomeStack" options={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="HomeStack" component={HomeStackNavigator} options={{ headerShown: false }}/>
+      
+      {/* Agrega aquí más pantallas si es necesario */}
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;

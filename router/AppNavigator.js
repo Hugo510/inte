b99de02aac6176379d0d85291b8f8bb6b583155a
andy contreras <zhchypyp@gmail.com>
2024-02-06 12:01:10 -0600

    another commit

--- router/AppNavigator.js
+++ router/AppNavigator.js
@@ -1,15 +1,17 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeStackNavigator from './HomeStackNavigator';
-import LoginScreen from '../views/LoginScreenV/LoginScreen';
-import RegisterScreen from '../views/registerScreen/RegisterScreen';
-import MQTTComponent from '../views/comunication/comunicationScreen';
-import CardsScreen from '../views/cards/cardsScreen';
-import DashboardScreen from '../views/dashboard/dashboardScreen';
-import { getUserRole } from '../utils/storage';
-import React, { useState, useEffect } from 'react';
-// import AdminScreen from '../path/to/AdminScreen';
+import {
+  createNativeStackNavigator,
+  NavigationContainer,
+} from "@react-navigation/native-stack";
+import React, { useEffect, useState } from "react";
+// import AdminScreen from "../path/to/AdminScreen";
+import { getUserRole } from "../utils/storage";
+import CardsScreen from "../views/cards/cardsScreen";
+import MQTTComponent from "../views/comunication/comunicationScreen";
+import DashboardScreen from "../views/dashboard/dashboardScreen";
+import LoginScreen from "../views/LoginScreenV/LoginScreen";
+import RegisterScreen from "../views/registerScreen/RegisterScreen";
+import HomeStackNavigator from "./HomeStackNavigator";
 // import MonitorScreen from '../path/to/MonitorScreen';
-
 
 //import ProfileScreen from '../views/profileScreen/ProfileScreen';
 
@@ -27,27 +29,50 @@ const AppNavigator = () => {
     fetchUserRole();
   }, []);
 
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="HomeStack">
-        {userRole === 'admin' ? (
+        {userRole === "admin" ? (
           <>
             <Stack.Screen name="AdminScreen" component={AdminScreen} />
             {/* Incluir aquí otras pantallas accesibles para admin */}
           </>
         ) : (
           <>
-            <Stack.Screen name="MonitorScreen" component={MonitorScreen} />
+            {/* <Stack.Screen name="MonitorScreen" component={MonitorScreen} /> */}
             {/* Incluir aquí otras pantallas accesibles para monitores */}
           </>
         )}
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="HomeStack" component={HomeStackNavigator} options={{ headerShown: false }}/>
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Api" component={MQTTComponent} options={{ headerShown: false }}/>
-        <Stack.Screen name="Cards" component={CardsScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ headerShown: false }}/>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeStack"
+          component={HomeStackNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Api"
+          component={MQTTComponent}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Cards"
+          component={CardsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{ headerShown: false }}
+        />
         {/* Agrega aquí más pantallas si es necesario */}
       </Stack.Navigator>
     </NavigationContainer>

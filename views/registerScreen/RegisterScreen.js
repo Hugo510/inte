b24a377d134222112137f455c34af4c77e94e4bb
Hugo Perez <hugoperez469@gmail.com>
2024-02-08 16:54:53 -0600

    Mejora register con campos y admin/user

--- views/registerScreen/RegisterScreen.js
+++ views/registerScreen/RegisterScreen.js
@@ -1,23 +1,36 @@
 // views/RegisterScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView, Platform, Button, Alert } from 'react-native';
-import styles from './RegisterScreen.styles'; 
-import DatePicker from '@react-native-community/datetimepicker'; // Asegúrate de instalar esta librería
-import { RadioButton, Checkbox  } from 'react-native-paper'; // Asegúrate de instalar esta librería
-//import CheckBox from '@react-native-community/checkbox'; // Asegúrate de instalar esta librería
+import { View, Text, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView, Platform, Button, Alert, ActivityIndicator } from 'react-native';
+import styles from './RegisterScreen.styles';
+import DatePicker from '@react-native-community/datetimepicker';
+import { RadioButton, Checkbox } from 'react-native-paper';
 
 
 const RegisterScreen = ({ navigation }) => {
-  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-    const [date, setDate] = useState(new Date());
-    const [gender, setGender] = useState('male');
-    const [checked, setChecked] = useState(false);
+  const [age, setAge] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [loading, setLoading] = useState(false); // Nuevo estado para manejar la carga
 
     // Función para manejar el cambio de fecha
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+    const validateForm = () => {
+      if (!email || !password || !age) {
+        Alert.alert('Error', 'Por favor, rellena todos los campos.');
+        return false;
+      }
+      if (isNaN(age) || age < 18) {
+        Alert.alert('Error', 'La edad debe ser un número y mayor o igual a 18.');
+        return false;
+      }
+      // Implementa aquí más validaciones según sea necesario
+      return true;
+    };
+  
+
 
   // Funciones para mostrar y ocultar el DatePicker
   const showDatePicker = () => {
@@ -35,37 +48,37 @@ const RegisterScreen = ({ navigation }) => {
   };
 
   const handleRegister = async () => {
-    // Crear el objeto de datos del usuario para enviar
+    if (!validateForm()) return; // Detener si la validación falla
+
+    setLoading(true); // Iniciar la carga
     const userData = {
-      name,
       email,
-      password, // Asegúrate de tener un campo de contraseña en tu formulario
+      password: password, // Considera usar hashing o alguna medida de seguridad aquí
+      age,
       birthDate: date.toISOString(),
-      gender,
-      hasPhone: checked,
+      isAdmin,
     };
+
+    const endpoint = isAdmin ? 'http://localhost:3000/api/admins/register' : 'http://localhost:3000/api/users/register';
   
-    try {
-      const response = await fetch('http://localhost:3000/api/admins/register', {
+    try  {
+      const response = await fetch(endpoint, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userData),
       });
-  
+
       const json = await response.json();
       if (response.ok) {
-        // Registro exitoso
         Alert.alert('Éxito', 'Usuario registrado correctamente');
-        // Opcional: Navegar a la pantalla de inicio de sesión o a la pantalla principal
+        // Implementa la navegación o la limpieza del formulario aquí
       } else {
-        // Error en el registro
         Alert.alert('Error', json.message || 'No se pudo registrar el usuario');
       }
     } catch (error) {
-      console.error(error);
       Alert.alert('Error', 'No se pudo conectar al servidor');
+    } finally {
+      setLoading(false); // Finalizar la carga
     }
   };
 
@@ -81,11 +94,11 @@ const RegisterScreen = ({ navigation }) => {
       <ImageBackground
         source={require('../../assets/images/background.jpg')} // Replace with your image path
         style={styles.imageContainer}
-      >
+      > 
         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
             <Text style={styles.navBack}>&lt;</Text>
           </TouchableOpacity>
-        
+          {loading && <ActivityIndicator size="large" color="#0000ff" />} {/* Indicador de carga */}
         <Text style={styles.title}>Air Guard.</Text>
         <Text style={styles.subtitle}>¿Listo para dar el paso?</Text>
         {/* You can add overlay content here */}
@@ -95,16 +108,19 @@ const RegisterScreen = ({ navigation }) => {
         <Text style={styles.header}>Registro</Text>
         <TextInput
           style={styles.input}
-          placeholder="Nombre"
-          value={name}
-          onChangeText={setName} // Actualiza el estado del nombre
-        />
-        <TextInput
-          style={styles.input}
           placeholder="Email"
           keyboardType="email-address"
           value={email}
           onChangeText={setEmail} // Actualiza el estado del email
+          autoCapitalize="none" // Mejora para evitar la capitalización automática
+          />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Edad"
+          keyboardType="numeric"
+          value={age}
+          onChangeText={setAge}
         />
 
         {/* DatePicker para la fecha de nacimiento */}
@@ -122,7 +138,7 @@ const RegisterScreen = ({ navigation }) => {
       </View>
 
           {/* RadioButton para seleccionar el género */}
-          <RadioButton.Group onValueChange={value => setGender(value)} value={gender}>
+{/*           <RadioButton.Group onValueChange={value => setGender(value)} value={gender}>
             <View>
               <Text>Male</Text>
               <RadioButton value="male" />
@@ -131,26 +147,24 @@ const RegisterScreen = ({ navigation }) => {
               <Text>Female</Text>
               <RadioButton value="female" />
             </View>
-          </RadioButton.Group>
+          </RadioButton.Group> */}
 
-          {/* CheckBox para el teléfono */}
-          <View style={styles.checkboxContainer}>
+        <View style={styles.checkboxContainer}>
           <Checkbox
-                            status={checked ? 'checked' : 'unchecked'}
-                            onPress={() => setChecked(!checked)}
-                            color={styles.checkboxColor} // Define tu color en styles
-                        />
-            <Text style={styles.checkboxLabel}>I have a phone</Text>
-          </View>
+            status={isAdmin ? 'checked' : 'unchecked'}
+            onPress={() => setIsAdmin(!isAdmin)}
+          />
+          <Text style={styles.checkboxLabel}>Es Usuario Administrador</Text>
+        </View>
 
         {/* Add other input fields here */}
         <View style={styles.buttonGroup}>
           <TouchableOpacity style={styles.cancelButton}>
             <Text style={styles.buttonText}>Cancel</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.confirmButton} onPress={handleRegister}>
-            <Text style={styles.buttonText}>Confirm</Text>
-          </TouchableOpacity>
+        <TouchableOpacity onPress={handleRegister} style={styles.confirmButton} disabled={loading}>
+          <Text style={styles.buttonText}>{loading ? 'Registrando...' : 'Confirmar'}</Text>
+        </TouchableOpacity>
         
       </View>
     </View>

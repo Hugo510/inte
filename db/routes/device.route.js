6119e11930aa044c97d59a0bc10e7aa009233bb8
Hugo Perez <hugoperez469@gmail.com>
2024-02-09 14:17:11 -0600

    Archivos faltantes

--- db/routes/device.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addDevice, getDevices } = require('../controller/device.controller.js');
-const router = express.Router();
-
-router.post('/addDevice', addDevice);
-router.get('/getDevices', getDevices);
-
-module.exports = router;
--- router/AppNavigator.js
+++ router/AppNavigator.js
@@ -1,4 +1,3 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeStackNavigator from './HomeStackNavigator';
 import LoginScreen from '../views/LoginScreenV/LoginScreen';
@@ -6,44 +5,14 @@ import RegisterScreen from '../views/registerScreen/RegisterScreen';
 import MQTTComponent from '../views/comunication/comunicationScreen';
 import CardsScreen from '../views/cards/cardsScreen';
 import DashboardScreen from '../views/dashboard/dashboardScreen';
-import { getUserRole } from '../utils/storage';
-import React, { useState, useEffect } from 'react';
-// import AdminScreen from '../path/to/AdminScreen';
-// import MonitorScreen from '../path/to/MonitorScreen';
-
-
-//import ProfileScreen from '../views/profileScreen/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userRole, setUserRole] = useState(null);
-
-  useEffect(() => {
-    const fetchUserRole = async () => {
-      const role = await getUserRole(); // Asumiendo que esta función retorna el rol del usuario almacenado localmente
-      setUserRole(role);
-    };
-
-    fetchUserRole();
-  }, []);
-
 
   return (
-    <NavigationContainer>
+    
       <Stack.Navigator initialRouteName="HomeStack">
-        {userRole === 'admin' ? (
-          <>
-            {/* <Stack.Screen name="AdminScreen" component={AdminScreen} /> */}
-            {/* Incluir aquí otras pantallas accesibles para admin */}
-          </>
-        ) : (
-          <>
-            {/* <Stack.Screen name="MonitorScreen" component={MonitorScreen} /> */}
-            {/* Incluir aquí otras pantallas accesibles para monitores */}
-          </>
-        )}
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="HomeStack" component={HomeStackNavigator} options={{ headerShown: false }}/>
         <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
@@ -52,7 +21,6 @@ const AppNavigator = () => {
         <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ headerShown: false }}/>
         {/* Agrega aquí más pantallas si es necesario */}
       </Stack.Navigator>
-    </NavigationContainer>
   );
 };
 

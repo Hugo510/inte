7a8bf0256d39a47348221bd19563973bd8aa89ab
Hugo510 <121467158+Hugo510@users.noreply.github.com>
2024-03-21 08:39:10 -0600

    Update admin.model.js

--- src/model/admin.model.js
+++ src/model/admin.model.js
@@ -30,7 +30,7 @@ const adminSchema = new mongoose.Schema({
         type: Date,
         required: true,
     },
-    profilePictureUrl: {
+    profilePictureId: {
         type: String,
     },
     role: {

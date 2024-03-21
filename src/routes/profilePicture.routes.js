c999218ad125861006f03cae20a3a3571cb51bbc
Hugo510 <121467158+Hugo510@users.noreply.github.com>
2024-03-21 08:48:33 -0600

    Update and rename profilePictureRoutes.js to profilePicture.routes.js

--- src/routes/profilePicture.routes.js
+++ src/routes/profilePicture.routes.js
@@ -4,6 +4,10 @@ const profilePictureController = require('../controller/profilePictureController
 
 // Rutas para cargar y recuperar imágenes de perfil
 router.post('/admin/:id/profile-picture', profilePictureController.uploadProfilePicture('Admin'), profilePictureController.saveProfilePicture);
-router.get('/admin/profile-picture/:id', profilePictureController.getProfilePicture);
+// Ruta para obtener la imagen de perfil de Admin
+router.get('/admin/profile-picture/:id', profilePictureController.getProfilePicture('Admin'));
+
+// Ruta para obtener la imagen de perfil de User
+router.get('/user/profile-picture/:id', profilePictureController.getProfilePicture('User'));
 
 router.post('/user/:id/profile-picture', profilePictureController.uploadProfilePicture('User'), profilePictureController
